031071bb745b40b0b47d130ecd3af9383da1035c
Igor Engler <53021313+igorengler97@users.noreply.github.com>
2022-05-18 18:22:13 -0300

    Update udp_server.py

--- udp_server.py
+++ udp_server.py
@@ -1,21 +1,22 @@
 import socket, tqdm, select, os, sys
-#from datetime import datetime
+from datetime import datetime
 
 SERVER_HOST = "0.0.0.0"
-SERVER_PORT = 5005
+SERVER_PORT = 5001
 TIMEOUT = 3
-BUFFER =  500
+BUFFER =  int(sys.argv[1]) #500
 
 
 def server_udp():
-    #tstart = datetime.now()
+    blocks = 0
+    
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((SERVER_HOST, SERVER_PORT))
 
     while True:
         data, address = server_socket.recvfrom(BUFFER)
 
-        print(f"[+] {address} is connected.")
+        print(f"{address} is connected.")
         if data:
 
             file_name, file_size = data.split(b"</>")
@@ -31,15 +32,20 @@ def server_udp():
                 if ready[0]:
                     data, address = server_socket.recvfrom(BUFFER)
                     f.write(data)
-                    
+                    blocks = blocks + 1
                 else:
                     f.close()
                     server_socket.close()
-                    #tend = datetime.now()
-                    #print (tend - tstart)
-                    sys.exit()
+                    #print(blocks)
+                    #TEND = datetime.now()
+                    #print (TEND - TSTART)
+                    return blocks
                 progress_bar.update(len(data))
+                
 
 if __name__ == "__main__":
-    server_udp()
+    #TSTART = datetime.now()
+    print(f"BLOCKS RECEIVED: {server_udp()}")
+    #TEND = datetime.now()
+    #print (TEND - TSTART)
     
