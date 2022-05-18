8f08906bd439d9b1bf07e48f2965cbd43c7c12a1
Igor Engler <53021313+igorengler97@users.noreply.github.com>
2022-05-18 18:21:33 -0300

    Update tcp_server.py

--- tcp_server.py
+++ tcp_server.py
@@ -1,51 +1,57 @@
-import socket
-import tqdm
-import os
+from fileinput import filename
+import socket, tqdm, os, sys
+from datetime import datetime
 # device's IP address
 SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 5001
-BUFFER = 4096 # recebe 4096 bytes por vez (mudar pra receber como comando)
-SEPARATOR = "</>"
+BUFFER = int(sys.argv[1])
 
+def server_tcp():
+    try:
+        #blocks = 0
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((SERVER_HOST, SERVER_PORT))
+
+        server_socket.listen(5)
+        print(f"Listening as {SERVER_HOST}:{SERVER_PORT}")
+
+        # aceita conexão, caso haja uma
+        client_socket, address = server_socket.accept() 
+        
+        # printa o endereço conectado
+        print(f"{address} is connected.")
+
+        # recebe as informações dos dados, utilizando socket do cliente
+        received = client_socket.recv(BUFFER).decode()
+        filename, filesize = received.split("</>")
+        
+        # remove caminho absoluto
+        filename = os.path.basename(filename) 
+    
+        # casting para inteiro
+        filesize = int(filesize)  
+
+        # recebe arquivo do socket 
+        progress_bar = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        with open(filename, "wb") as f:
+            while True:
+                data = client_socket.recv(BUFFER)
+                if not data:    
+                    #se nada for recebido, a transmissão termina
+                    break
+                f.write(data)
+                # update the progress bar
+                progress_bar.update(len(data))
+                #blocks = blocks + 1
+
+    finally:
+        # fecha o socket do cliente
+        client_socket.close()
+        # fecha o socket do server
+        server_socket.close()
+
+        return
 
 if __name__ == "__main__":
-    
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    s.bind((SERVER_HOST, SERVER_PORT))
-
-    s.listen(5)
-    print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
-
-    # aceita conexão, caso haja uma
-    client_socket, address = s.accept() 
-    
-    # printa o endereço conectado
-    print(f"[+] {address} is connected.")
-
-    # recebe as informações dos dados, utilizando socket do cliente
-    received = client_socket.recv(BUFFER).decode()
-    filename, filesize = received.split(SEPARATOR)
-    
-    # remove caminho absoluto
-    filename = os.path.basename(filename) 
-   
-    # casting para inteiro
-    filesize = int(filesize)  
-
-    # recebe arquivo do socket 
-    progress_bar = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=BUFFER)
-    with open(filename, "wb") as f:
-        while True:
-            data = client_socket.recv(BUFFER)
-            if not data:    
-                #se nada for recebido, a transmissão termina
-                break
-            f.write(data)
-            # update the progress bar
-            progress_bar.update(len(data))
-
-    # fecha o socket do cliente
-    client_socket.close()
-    # fecha o socket do server
-    s.close()
+    #TSTART = datetime.now()
+    server_tcp()
