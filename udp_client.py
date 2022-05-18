a463cf6aefa64445a127759a744d383ca3645531
Igor Engler <53021313+igorengler97@users.noreply.github.com>
2022-05-18 18:21:57 -0300

    Update udp_client.py

--- udp_client.py
+++ udp_client.py
@@ -2,14 +2,14 @@ import socket, time, os, sys, tqdm
 from datetime import datetime
 
 HOST = "localhost"
-PORT = 5005
-BUFFER = 500
+PORT = 5001
 FILENAME = sys.argv[1]
 FILESIZE = os.path.getsize(FILENAME) 
-
+BUFFER = int(sys.argv[2])
 
 def client_udp():
     try:
+        blocks = 0
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         #sock.sendto(FILENAME.encode(), (UDP_IP, UDP_PORT))
         sock.sendto(f"{FILENAME}</>{FILESIZE}".encode(), (HOST, PORT))
@@ -20,16 +20,19 @@ def client_udp():
 
         progress_bar = tqdm.tqdm(range(FILESIZE), f"Sending {FILENAME}", unit="B", unit_scale=True, unit_divisor=1024)
         while(data):
+            #blocks2 = blocks2 + 1
             if(sock.sendto(data, (HOST, PORT))):
                 data = f.read(BUFFER)
                 progress_bar.update(len(data))
-                #time.sleep(0.005) # Give receiver a bit time to save
+                blocks = blocks + 1
+                #time.sleep(0.02) # Give receiver a bit time to save
     finally:
         sock.close()
         f.close()
+        return blocks
 
 if __name__ == "__main__":
     tstart = datetime.now()
-    client_udp()
+    print(f"BLOCKS GENERATED: {client_udp()}")
     tend = datetime.now()
-    print (tend - tstart)
+    print (f"TIME ELAPSED: {(tend - tstart).total_seconds()*1000}")
